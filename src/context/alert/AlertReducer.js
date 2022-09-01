f4e87d3305d17e49aa43f5d7787ad15c03dd1c72
Cory Simpson <cory.simpson.dev@gmail.com>
2022-09-01 13:13:45 -0700

    initial build

--- src/context/alert/AlertReducer.js
+++ src/context/alert/AlertReducer.js
@@ -1,11 +1,11 @@
-function alertReducer(state, action) {
-  switch(action.type) {
+const alertReducer = (state, action) => {
+  switch (action.type) {
     case 'SET_ALERT':
-        return action.payload
+      return action.payload
     case 'REMOVE_ALERT':
-        return null
+      return null
     default:
-        return state
+      return state
   }
 }
 
